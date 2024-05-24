8fc84d5f0a162f59f8b57ad590df870b8d8efdf8
Jin Park <jinpark1504@gmail.com>
2024-05-24 15:23:46 +0900

    Todo List with adding, editing, and removing

--- TodoList/AddTodoViewController.swift
+++ TodoList/AddTodoViewController.swift
@@ -7,23 +7,52 @@
 
 import UIKit
 
+protocol AddTodoViewControllerDelegate {
+    func save(todoItem: TodoItem)
+}
 class AddTodoViewController: UIViewController {
 
+    var delegate: AddTodoViewControllerDelegate?
+    var prioritySegment: UISegmentedControl!
+    var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "Add Todo"
+        
+        view.backgroundColor = .white
+        prioritySegment = UISegmentedControl(items: Priority.allCases.map { $0.rawValue })
+        prioritySegment!.selectedSegmentIndex = 0
+        view.addSubview(prioritySegment!)
+        
+        prioritySegment.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField = UITextField()
+        textField.placeholder = "Enter todo title"
+        textField.borderStyle = .roundedRect
+        view.addSubview(textField)
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            prioritySegment.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            prioritySegment.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: -16),
+            textField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 10),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        let rightButton = UIBarButtonItem(title: "Add", style: .done, target: self, action: #selector(addTodo))
+        self.navigationItem.rightBarButtonItem = rightButton
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addTodo() {
+        guard let title = textField.text, !title.isEmpty else { return }
+        
+        let selectedPriority = Priority.allCases[prioritySegment.selectedSegmentIndex]
+        let newItem = TodoItem(id: UUID(), title: textField.text!, isCompleted: false, priority: selectedPriority)
+        delegate?.save(todoItem: newItem)
+        navigationController?.popViewController(animated: true)
     }
-    */
-
 }
--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -7,13 +7,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AddTodoViewControllerDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableView: UITableView!
+    var todoItems: [Priority: [TodoItem]] = [.low: [], .medium: [], .high: []]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
+        setupNavigationBar()
     }
-
-
+    
+    func setupTableView() {
+        tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Todo List"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTodoItem))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddTodoScreen))
+    }
+    
+    @objc func editTodoItem() {
+        tableView.setEditing(!tableView.isEditing, animated: true)
+    }
+    @objc func showAddTodoScreen() {
+        let addTodoViewController = AddTodoViewController()
+        addTodoViewController.delegate = self
+        navigationController?.pushViewController(addTodoViewController, animated: true)
+    }
+    
+    func save(todoItem: TodoItem) {
+        todoItems[todoItem.priority]?.append(todoItem)
+        tableView.reloadData()
+    }
+    
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Priority.allCases.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let priority = Priority.allCases[section]
+        return todoItems[priority]?.count ?? 0
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Priority.allCases[section].rawValue
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let priority = Priority.allCases[indexPath.section]
+        if let item = todoItems[priority]?[indexPath.row] {
+            cell.textLabel?.text = item.title
+            cell.accessoryType = item.isCompleted ? .checkmark : .none
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let priority = Priority.allCases[indexPath.section]
+        if var item = todoItems[priority]?[indexPath.row] {
+            item.isCompleted.toggle()
+            todoItems[priority]?[indexPath.row] = item
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let priority = Priority.allCases[indexPath.section]
+            todoItems[priority]?.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
 
